898dbd72554540f6bf6ea63eb21beb6e893bd369
zhoubin <525589458@qq.com>
2023-07-04 15:19:56 +0800

    重新布局

--- src/main/java/com/timemachine/ui/App.java
+++ src/main/java/com/timemachine/ui/App.java
@@ -9,13 +9,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -29,17 +28,55 @@ public class App extends Application {
     private VBox left;
 
     @FXML
+    private ListView<String> listView;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @FXML
     private TableView<Backuproot> center;
 
     @FXML
+    private HBox content1;
+    public void initialize() {
+
+        content1.setVisible(true);
+//        ListView<String> listView = new ListView<>();
+//        listView.getStyleClass().add("custom-list-view");
+//        listView.setEditable(true);
+        listView.setCellFactory(lv -> {
+            TextFieldListCell<String> cell = new TextFieldListCell<String>() {
+                @Override
+                public void startEdit() {
+                    super.startEdit();
+                    setPrefHeight(30); // 设置编辑框的高度
+                }
+            };
+
+            cell.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && !cell.isEmpty()) {
+                    listView.edit(listView.getSelectionModel().getSelectedIndex());
+                }
+            });
+            return cell;
+        });
+        listView.getItems().addAll(
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+        );
+
+    }
+    @FXML
     private void onMenuButtonClick(MouseEvent event) throws Exception {
         ToggleButton clickedButton = (ToggleButton) event.getSource();
-//        new Add().start(new Stage());
-        // 切换按钮的选中状态
         clickedButton.setSelected(!clickedButton.isSelected());
         System.out.println("666");
-//        clickedButton.getStyleClass().add("menu-button-select");
-//        System.out.println(clickedButton.getStyleClass());
+//        Button button1 = new Button();
+//        System.out.println(borderPane);
+//        borderPane.setCenter(button1);
         // 更新样式
         left.getChildren().forEach(node -> {
             if (node instanceof ToggleButton) {
@@ -48,21 +85,10 @@ public class App extends Application {
             }
         });
         clickedButton.getStyleClass().add("menu-button-select");
-//        if (clickedButton.isSelected()) {
-//            System.out.println("666");
-//            clickedButton.getStyleClass().add("menu-button-select");
-//        } else {
-//            System.out.println("777");
-//            System.out.println(clickedButton.getStyleClass());
-//            clickedButton.getStyleClass().add("menu-button-select");
-////            clickedButton.getStyleClass().remove("menu-button-selected");
-//        }
     }
     @FXML
     protected void onHelloButtonClick() {
-//        System.out.println("点击了");
-//        left.getChildren();
-//        welcomeText.setText("Welcome to JavaFX Application!");
+        //
     }
     @Override
     public void start(Stage stage) throws IOException {
@@ -71,38 +97,6 @@ public class App extends Application {
         System.out.println(root);
         Scene scene = new Scene(root, 700, 500);
         scene.getStylesheets().add(App.class.getResource("/css/style.css").toExternalForm());
-
-        //中 表格
-//        TableView<Backuproot> table=new TableView<>();
-//
-//        List<TableColumn<Backuproot, ?>> columns = new ArrayList<>();
-//        TableColumn<Backuproot,Integer> c1=new TableColumn<>("ID");
-//        TableColumn<Backuproot,String> c2=new TableColumn<>("路径");
-//
-//        c1.setCellValueFactory(new PropertyValueFactory<>("id"));
-//        c2.setCellValueFactory(new PropertyValueFactory<>("rootpath"));
-//        columns.add(c1);
-//        columns.add(c2);
-//        table.getColumns().addAll(columns);
-
-        // 创建ObservableList对象并添加数据
-//        ObservableList<Backuproot> data = FXCollections.observableArrayList();
-//        ObservableList<Backuproot> all = BackuprootDao.all();
-//        System.out.println(all);
-//        data.add(new Backuproot(1, "John"));
-//        data.add(new Backuproot(2, "Jane"));
-//        data.add(new Backuproot(3, "Mike"));
-//        table.setItems(all);
-
-//        root.setCenter(table);
-
-        // 创建数据列表
-//        ObservableList<String> data = FXCollections.observableArrayList(
-//                "Item 1", "Item 2", "Item 3", "Item 4", "Item 5");
-//
-//        // 创建ListView并设置数据源
-//        ListView<String> listView = new ListView<>(data);
-//        root.setCenter(listView);
         stage.setScene(scene);
         stage.show();
     }
